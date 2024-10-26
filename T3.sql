8d0e6c70b69401d9dc015484a4d8dda60123c817
Alter6889 <etereolaider@gmail.com>
2024-10-26 01:02:28 -0500

    inicio

---
+++ T3.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE GREENBUS
+USE DATABASE GREENBUS
+CREATE TABLE bus (
+idbus int PRIMARY KEY,
+empresa VARCHAR (30),
+calificacion int,
+Destino VARCHAR (40),
+Inicio VARCHAR (40),
+fecviaje VARCHAR (8),
+Precio double,
+NroDisponible int,
+)
+CREATE TABLE clientes(
+idcliente int PRIMARY KEY,
+nomcli VARCHAR (50),
+dnicli VARCHAR (8),
+telefono VARCHAR (9),
+edad int,
+DirCli VARCHAR (70),
+)
+Create Table Reserva(
+idRes int PRIMARY KEY,
+NroRes int,
+metodopago VARCHAR (25),
+idcliente int FOREIGN KEY REFERENCES clientes(idcliente),
+nomcli VARCHAR(50),
+dnicli VARCHAR(8),
+idbus int FOREIGN KEY REFERENCES bus(idbus),
+empresa VARCHAR(30),
+Destino VARCHAR (40),
+Inicio VARCHAR (40),
+fecviaje VARCHAR (8),
+Precio double,
+
+
+)
